5b7542386576fc3395a1a5c87f4d718c5eadddac
Arsh <kau15140@sheridancollege.ca>
2023-05-18 15:16:32 -0400

    My first arsh commit

---
+++ src/githelloworld/Githelloworld.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githelloworld;
+
+/**
+ *
+ * @author arshc
+ */
+public class Githelloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
+        v
+                v
+        
+    }
+    
+}
